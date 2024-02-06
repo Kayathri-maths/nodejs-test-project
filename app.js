c61152f122fcbb5c6da13b28b354d4c721b2d7c3
Kayathri-maths <kayualagarsamy@gmail.com>
2024-02-06 21:09:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const sequelize = require('./util/database');
+
+const data = require('./models/data');
+
+var cors = require('cors');
+
+const app = express();
+
+const dataRoutes = require('./routes/data');
+
+app.use(cors());
+
+app.use(bodyParser.json({ extended: false}));
+
+app.use('/data',dataRoutes);
+
+app.use(errorController.get404);
+
+sequelize.sync()
+  .then(res =>{
+   app.listen(3000);
+})
+ .catch( err => console.log(err));
+
+
+
+
